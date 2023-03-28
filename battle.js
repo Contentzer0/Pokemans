28178e6cb4917b86b51785305eed097f4fbd097c
Conzer <119615637+Contentzer0@users.noreply.github.com>
2023-03-28 10:58:57 -0400

    mvp plus more

--- battle.js
+++ battle.js
@@ -2,8 +2,15 @@ const p1Life = document.querySelector('.p1Life')
 const p2Life = document.querySelector('.p2Life')
 const leer = document.querySelector(".leer")
 const tackle = document.querySelector(".tackle")
-const text = document.querySelector(".text")
-const specials = document.querySelector(".specials")
+let text = document.querySelector(".text")
+let specials = document.querySelector(".specials")
+let counter = 0
+const box = document.getElementsByClassName("delay")
+let pika = document.querySelector(".text1")
+let spec = document.querySelector(".specials")
+let spec1 = document.querySelector(".p2Life")
+let spec2 = spec1.innerText
+let spec3 = parseInt(spec2)
 function getChoice() {
     let name2 = JSON.parse(sessionStorage.getItem("Pokemon"))
     let name1 = name2.name
@@ -24,11 +31,83 @@ getChoice();
 
 specials.addEventListener("click", function() {
     text.innerHTML = "You deal damage?"
-})
+    setTimeout(() => {    
+        // 👇️ removes element from DOM
+        text.innerHTML = '<span class="delay"></span>'
+    
+        // 👇️ hides element (still takes up space on page)
+        // box.style.visibility = 'hidden';
+      }, 1000);
+});
+
 tackle.addEventListener("click", function() {
-    text.innerHTML = "You tickle the Bingus"
-})
+    text.innerHTML = "You tickle the Chungus"
+    spec3 += 1
+    spec1.innerText = spec3.toString()
+    setTimeout(() => {    
+        // 👇️ removes element from DOM
+        text.innerHTML = '<span class="delay"></span>'
+    
+        // 👇️ hides element (still takes up space on page)
+        // box.style.visibility = 'hidden';
+      }, 1000);
+});
+
 leer.addEventListener("click", function() {
     text.innerHTML = "Your gazes only make him stronger"
-})
+    spec3 += 3
+    spec1.innerText = spec3.toString()
+    setTimeout(() => {    
+        // 👇️ removes element from DOM
+        text.innerHTML = '<span class="delay"></span>'
+    
+        // 👇️ hides element (still takes up space on page)
+        // box.style.visibility = 'hidden';
+      }, 1000);
+});
 
+spec.addEventListener("click", function () {
+    if (spec3 > 0) {
+        spec3 -= 2
+        spec1.innerText = spec3.toString()
+    }
+})
+let spec4 = document.querySelector(".p1Life")
+let spec5 = spec4.innerText
+let spec6 = parseInt(spec5)
+let cycle = document.querySelector(".life")
+function incrementCounter() {
+    counter ++
+    if (counter == 1) {
+        pika.innerHTML = "YOU FOOL !"
+        setTimeout(() => {    
+            // 👇️ removes element from DOM
+            pika.innerHTML = ""
+        
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 2000);
+    } else if (counter == 2) {
+        pika.innerHTML = "I will destroy you"
+        setTimeout(() => {    
+            // 👇️ removes element from DOM
+            pika.innerHTML = ""
+        
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+            }, 2000);
+    } else if (counter == 3) {
+        pika.innerHTML = "Guuuuurl, better back up"
+        setTimeout(() => {    
+            // 👇️ removes element from DOM
+            pika.innerHTML = ""
+        
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 2000);
+    } else if (counter == 4) {
+        spec6 -= 25
+        spec4.innerText = spec6.toString()
+        cycle.classList.remove("life")
+    }
+}
